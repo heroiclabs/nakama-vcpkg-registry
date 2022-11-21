b05602841910bdbbee19ff3eaf709b6536d10d5f
Luke Gehorsam <luke@heroiclabs.com>
2022-11-21 07:24:06 -0500

    don't use system python

--- ports/cocos2d-x/portfile.cmake
+++ ports/cocos2d-x/portfile.cmake
@@ -6,15 +6,16 @@ vcpkg_from_git(
     HEAD_REF v4
 )
 
+find_package(python2 NO_SYSTEM_ENVIRONMENT_PATH)
+
 set(VCPKG_USE_HEAD_VERSION OFF)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E echo "yes" # accept metal support
-     COMMAND "python" "download-deps.py"
+     COMMAND ${python2_BINARY_DIR)/python.exe "download-deps.py"
      WORKING_DIRECTORY ${SOURCE_PATH}
      COMMAND_ECHO STDOUT ECHO_OUTPUT_VARIABLE ECHO_ERROR_VARIABLE COMMAND_ERROR_IS_FATAL ANY)
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
-
 
 vcpkg_cmake_config_fixup()
 vcpkg_cmake_install()
