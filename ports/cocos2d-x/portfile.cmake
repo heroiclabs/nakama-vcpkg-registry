8bf3dfb09e9a5e5cefeb96fe3d3241af2f3d9f9f
Luke Gehorsam <luke@heroiclabs.com>
2022-11-25 14:50:35 -0500

    use add_subdirectory workaround

--- ports/cocos2d-x/portfile.cmake
+++ ports/cocos2d-x/portfile.cmake
@@ -1,4 +1,8 @@
 set(VCPKG_USE_HEAD_VERSION ON)
+message("CURRENT_BUILDTREES_DIR ${CURRENT_BUILDTREES_DIR}")
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR})
+
+
 
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -15,11 +19,9 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E echo "yes" # accept metal support
      WORKING_DIRECTORY ${SOURCE_PATH}
      COMMAND_ECHO STDOUT ECHO_OUTPUT_VARIABLE ECHO_ERROR_VARIABLE COMMAND_ERROR_IS_FATAL ANY)
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS
-        -DBUILD_LUA_LIBS=OFF
-        -DBUILD_CPP_TEMPLATE=OFF
-        -DBUILD_CPP_TESTS=OFF
-)
+file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/cocos2d-x-src)
 
-vcpkg_cmake_install()
+### cocos is not installable so we resort to an add_subdirectory workaround.
+file(COPY ${SOURCE_PATH} DESTINATION ${CURRENT_BUILDTREES_DIR})
+get_filename_component(SOURCE_NAME ${SOURCE_PATH} NAME)
+file(RENAME ${CURRENT_BUILDTREES_DIR}/${SOURCE_NAME} ${CURRENT_BUILDTREES_DIR}/cocos2d-x-src)
