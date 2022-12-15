9a381b48a69ef8c73cf630f5fc3a569e375b8536
Luke Gehorsam <luke@heroiclabs.com>
2022-12-14 19:13:35 -0500

    do config fixup after vcpkg_cmake_install

--- ports/nakama-sdk/portfile.cmake
+++ ports/nakama-sdk/portfile.cmake
@@ -26,7 +26,7 @@ vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
 )
 
 
-vcpkg_cmake_config_fixup()
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
