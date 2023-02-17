034792214ce88a4cf0dfc7ae6abbd6278bf825d3
Luke Gehorsam <luke@heroiclabs.com>
2023-02-17 15:55:17 -0500

    nakama-sdk v2.7.0

--- ports/nakama-sdk/portfile.cmake
+++ ports/nakama-sdk/portfile.cmake
@@ -1,12 +1,8 @@
-set(VCPKG_USE_HEAD_VERSION ON)
-
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/heroiclabs/nakama-cpp.git
-    HEAD_REF master
+    REF cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
 )
-
-set(VCPKG_USE_HEAD_VERSION OFF)
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
