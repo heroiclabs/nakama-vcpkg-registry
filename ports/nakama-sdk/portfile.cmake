3920da76bd92c33856c700230413b7972cc34ac2
Luke Gehorsam <luke@heroiclabs.com>
2023-05-02 16:08:10 -0400

    allow generator specification

--- ports/nakama-sdk/portfile.cmake
+++ ports/nakama-sdk/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/heroiclabs/nakama-cpp.git
-    REF a82046ee4d6d2142ba2ae7a5d2e31c5dcebe8daf
+    REF 166e83cd863b0d7957c9a5d6521b89cf3848ab53
 )
 
 vcpkg_check_features(
@@ -15,9 +15,19 @@ vcpkg_check_features(
         libhttpclient-http WITH_LIBHTTPCLIENT_HTTP
         libhttpclient-ws WITH_LIBHTTPCLIENT_WS
         cpprestsdk WITH_CPPRESTSDK
+        visual-studio VISUAL_STUDIO_GENERATOR
 )
 
+set(GENERATOR "Ninja Multi-Config")
+
+if (VISUAL_STUDIO_GENERATOR)
+    set(GENERATOR "Visual Studio 17 2022")
+endif()
+
+message("Configuring Nakama with generator: ${GENERATOR}")
+
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
+  GENERATOR ${GENERATOR}
   OPTIONS
     -DDISTRIBUTE_OPTIONAL_LITE=OFF ### vcpkg will handle the distribution rather than the SDK itself
     ${FEATURE_OPTIONS} ### created by vcpkg_check_features
