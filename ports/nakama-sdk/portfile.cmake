16886190df2e0b90563ddec5ee93371596bd7621
Luke Gehorsam <luke@heroiclabs.com>
2023-05-05 15:35:50 -0400

    make windows-use-msbuild conditional

--- ports/nakama-sdk/portfile.cmake
+++ ports/nakama-sdk/portfile.cmake
@@ -15,11 +15,17 @@ vcpkg_check_features(
         libhttpclient-http WITH_LIBHTTPCLIENT_HTTP
         libhttpclient-ws WITH_LIBHTTPCLIENT_WS
         cpprestsdk WITH_CPPRESTSDK
-        visual-studio VISUAL_STUDIO_GENERATOR
+        windows-use-msbuild WINDOWS_USE_MSBUILD
 )
 
+if(WINDOWS_USE_MSBUILD)
+    set(WINDOWS_USE_MSBUILD_OPTION "WINDOWS_USE_MSBUILD")
+else()
+    set(WINDOWS_USE_MSBUILD_OPTION "")
+endif()
+
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
-  WINDOWS_USE_MSBUILD
+  ${WINDOWS_USE_MSBUILD_OPTION}
   OPTIONS
     -DDISTRIBUTE_OPTIONAL_LITE=OFF ### vcpkg will handle the distribution rather than the SDK itself
     ${FEATURE_OPTIONS} ### created by vcpkg_check_features
