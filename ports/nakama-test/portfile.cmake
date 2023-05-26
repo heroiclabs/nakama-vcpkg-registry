8a1159ded5fa7e9e8a6afcf6faa46c08ca0e7f2f
Luke Gehorsam <luke@heroiclabs.com>
2023-05-26 09:45:37 -0400

    new test version

--- ports/nakama-test/portfile.cmake
+++ ports/nakama-test/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/heroiclabs/nakama-cpp.git
-    REF f71339aa6912a81c649243a2f395005d34372f9d
+    REF 912d17f1b3f0c989c2714a8d57fb1bd5b26a0727
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}/test)
