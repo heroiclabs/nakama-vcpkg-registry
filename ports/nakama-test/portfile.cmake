a5e59928a1f0b807f135b98fa124763017874b09
Luke Gehorsam <luke@heroiclabs.com>
2023-06-03 08:29:46 -0400

    update test hash

--- ports/nakama-test/portfile.cmake
+++ ports/nakama-test/portfile.cmake
@@ -1,13 +1,20 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/heroiclabs/nakama-cpp.git
-    REF d127205a7df8513a03335e9ed6b55ef7b7dd9d03
+    REF ddd48a9ff926ce9b06e9afbd80b6d4146c3cc377
 )
+
+set(BUILD_SHARED_NAKAMA_SDK ON)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(BUILD_SHARED_NAKAMA_SDK OFF)
+endif()
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}/test
     OPTIONS
         -DREMOTE_NAKAMA_SDK=ON ### use vcpkg to get nakama rather than looking for a local version.
         -DBUILD_EXECUTABLE=OFF
+        -DBUILD_SHARED_NAKAMA_SDK=${BUILD_SHARED_NAKAMA_SDK}
 )
 
 vcpkg_cmake_install()
