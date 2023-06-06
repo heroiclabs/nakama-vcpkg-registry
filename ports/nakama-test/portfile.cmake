a0678eec73bcf9b376b7af4771b0ab3c1c264160
Luke Gehorsam <luke@heroiclabs.com>
2023-06-06 15:20:09 -0400

    force dll import export for nakama-test

--- ports/nakama-test/portfile.cmake
+++ ports/nakama-test/portfile.cmake
@@ -1,13 +1,20 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/heroiclabs/nakama-cpp.git
-    REF 3c9fadd82c456696de2816e576cb2048040a9944
+    REF 840b7b950b77be1cbfc0a874997ef41981b8a40c
+)
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        force-dll-import-export FORCE_DLL_IMPORT_EXPORT
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}/test
     OPTIONS
         -DREMOTE_NAKAMA_SDK=ON ### use vcpkg to get nakama rather than looking for a local version.
         -DBUILD_EXECUTABLE=OFF
+        ${FEATURE_OPTIONS} ### created by vcpkg_check_features
 )
 
 vcpkg_cmake_install()
